a3ac2567979578d72d9e592defc651cd4803091d
dyun <zkdlxkxks@nate.com>
2023-03-30 21:56:21 +0900

    add: change model type function ( h5 -> tflite )

---
+++ models/change_model_type.py
@@ -0,0 +1,14 @@
+import tensorflow as tf
+import efficientnet.keras as efn
+import sys
+
+if len(sys.argv) != 2:
+    print(f"Usage : {sys.argv[0]} h5_file")
+    exit(0)
+
+file_name = sys.argv[1]
+model = tf.keras.models.load_model(file_name)
+converter = tf.lite.TFLiteConverter.from_keras_model(model)
+tflite_model = converter.convert()
+open( file_name.split('.')[0] + ".tflite", "wb").write(tflite_model)
+
